e5d4feb687e3258076b2441d524421a34a9f1488
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-25 02:19:00 +0000

    Create flake for developing and building

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+	description = " ";
+
+	outputs = { self, crane, fenix, flake-utils, ... } @ inputs: 
+		flake-utils.lib.eachDefaultSystem (system: let 
+			rust-analyzer = fenix.packages.${system}.stable.rust-analyzer; 
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      toolchain = with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+						complete.rustfmt
+						complete.clippy
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+          ];
+
+			craneLib = crane.lib.${system}.overrideToolchain toolchain;
+	
+			commonArgs = {
+				src = ./.;
+				doCheck = false;
+			};
+	
+			cargoArtifacts = craneLib.buildDepsOnly commonArgs; 
+		in {
+			packages = {
+				default = craneLib.buildPackage commonArgs;
+				windows = craneLib.buildPackage commonArgs // {
+         CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+				};
+			};
+
+			checks = {
+				clippy = craneLib.cargoClippy commonArgs // { inherit cargoArtifacts; };
+				format = craneLib.cargoFmt commonArgs // { inherit cargoArtifacts; };
+			};
+
+			devShells = craneLib.devShell {
+				checks = self.checks.${system};
+				packages = [ rust-analyzer ];
+			};
+	});
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+		
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+		fenix = {
+		  url = "github:nix-community/fenix/monthly";
+     	inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+}
