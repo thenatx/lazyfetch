adb77a03718c278d8f38168d0935364e3442e6ab
NatProgramer <rodrigezsantiago40@gmail.com>
2024-07-10 19:46:25 +0000

    complete uptime module function

--- src/info/uptime.rs
+++ src/info/uptime.rs
@@ -2,20 +2,28 @@ use sysinfo::System;
 
 use crate::config::Uptime;
 
-const HOURS_IN_DAYS: u64 = 24;
-const MINUTES_IN_HOURS: u64 = 60;
-const SECONDS_IN_DAYS: u64 = 86400;
+const SECONDS_IN_HOURS: f64 = 3600.0;
+const SECONDS_IN_MINUTES: f64 = 60.0;
+const SECONDS_IN_DAYS: f64 = 86400.0;
 
 pub fn uptime(config: &Uptime) -> String {
-    let uptime_secs = System::uptime();
+    let uptime_secs = System::uptime() as f64;
 
-    let days = uptime_secs / SECONDS_IN_DAYS;
-    let hours = days / HOURS_IN_DAYS;
-    let minutes = hours / MINUTES_IN_HOURS;
+    let days = (uptime_secs / SECONDS_IN_DAYS) as u64;
+    let hours = (uptime_secs % SECONDS_IN_DAYS) / SECONDS_IN_HOURS;
+    let minutes = ((uptime_secs % SECONDS_IN_DAYS) % SECONDS_IN_HOURS) / SECONDS_IN_MINUTES;
 
     if config.shorthand.unwrap() {
-        return format!("{} d {} hrs {} mins", days, hours, minutes);
+        if days > 0 {
+            return format!("{}d {}h {}m", days as u64, hours as u64, minutes as u64);
+        }
+
+        return format!("{}h {}m", hours, minutes);
     }
 
-    format!("{} days {} hours {} minutes", days, hours, minutes)
+    if days > 0 {
+        return format!("{} days {} hours {} minutes", days, hours, minutes);
+    }
+
+    format!("{} hours {} minutes", hours, minutes)
 }
