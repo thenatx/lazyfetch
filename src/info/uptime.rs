9b87691a0966f5c7e8f57afa6901b1caceda6f0d
NatProgramer <rodrigezsantiago40@gmail.com>
2024-07-14 20:58:55 +0000

    fix: ugly output because when shorthand option are disabled

--- src/info/uptime.rs
+++ src/info/uptime.rs
@@ -10,20 +10,35 @@ pub fn uptime(config: &Uptime) -> String {
     let uptime_secs = System::uptime() as f64;
 
     let days = (uptime_secs / SECONDS_IN_DAYS) as u64;
-    let hours = (uptime_secs % SECONDS_IN_DAYS) / SECONDS_IN_HOURS;
-    let minutes = ((uptime_secs % SECONDS_IN_DAYS) % SECONDS_IN_HOURS) / SECONDS_IN_MINUTES;
+    let hours = ((uptime_secs % SECONDS_IN_DAYS) / SECONDS_IN_HOURS) as u64;
+    let minutes =
+        (((uptime_secs % SECONDS_IN_DAYS) % SECONDS_IN_HOURS) / SECONDS_IN_MINUTES) as u64;
 
     if config.shorthand.unwrap() {
-        if days > 0 {
-            return format!("{}d {}h {}m", days as u64, hours as u64, minutes as u64);
+        if days == 0 && hours > 0 {
+            return format!("{}h {}m", hours, minutes);
+        } else if days == 0 && hours == 0 {
+            return format!("{}m", minutes);
         }
 
-        return format!("{}h {}m", hours, minutes);
+        if hours == 0 && days > 0 {
+            return format!("{}d {}m", days, minutes);
+        }
+
+        return format!("{}d {}h {}m", days, hours, minutes);
     }
 
-    if days > 0 {
-        return format!("{} days {} hours {} minutes", days, hours, minutes);
+    if days == 0 && hours == 0 {
+        return format!("{} minutes", minutes);
     }
 
-    format!("{} hours {} minutes", hours, minutes)
+    if days == 0 && hours > 0 {
+        return format!("{} hours, {} minutes", hours, minutes);
+    }
+
+    if hours == 0 && days > 0 {
+        return format!("{} days, {} minutes", days, minutes);
+    }
+
+    format!("{} days, {} hours, {} minutes", days, hours, minutes)
 }
