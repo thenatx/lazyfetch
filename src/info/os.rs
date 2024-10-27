4b8ecb756580054c9c9f78f6c8d5517b34c3f0a1
NatProgramer <rodrigezsantiago40@gmail.com>
2024-10-27 09:33:28 -0500

    Implement the configuration options for the os var

--- src/info/os.rs
+++ src/info/os.rs
@@ -2,21 +2,32 @@ use super::ModuleVar;
 use crate::{config::OsConfig, error};
 
 pub struct OsVar<'a> {
-    name: &'a str, // Name of the var to use un config (like in "Hello from ${os}")
-    value: String, // This value is used to replace var while parsing, for example: "${os}" to "Arch linux"
+    pub name: &'a str, // Name of the var to use un config (like in "Hello from ${os}")
 }
 
 impl<'a> ModuleVar<OsConfig> for OsVar<'a> {
     fn new() -> Self {
-        Self {
-            name: "os",
-            value: String::new(),
-        }
+        Self { name: "os" }
     }
 
-    fn value(&mut self, cfg: Option<OsConfig>) {
-        let _cfg: OsConfig = cfg.unwrap();
-        let option_value = sysinfo::System::name();
-        self.value = error::handle_empty_var(option_value);
+    fn value(&mut self, cfg: Option<&OsConfig>) -> String {
+        let cfg: &OsConfig = cfg.unwrap();
+
+        let name = sysinfo::System::name();
+        let version = if cfg.shorthand.unwrap_or_default() {
+            sysinfo::System::long_os_version()
+        } else {
+            sysinfo::System::os_version()
+        };
+
+        let name = error::handle_empty_var(name);
+        let version = error::handle_empty_var(version);
+
+        if cfg.show_arch.unwrap_or_default() {
+            format!("{} {}", name, version)
+        } else {
+            let arch = error::handle_empty_var(sysinfo::System::cpu_arch());
+            format!("{} {} ({})", name, version, arch)
+        }
     }
 }
