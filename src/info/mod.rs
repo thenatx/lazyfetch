d4ff149ca4a9abc733cebf4301eaae1696bb3737
NatProgramer <rodrigezsantiago40@gmail.com>
2024-11-02 22:36:52 -0500

    Implement the function of use the user configuration for each module

--- src/info/mod.rs
+++ src/info/mod.rs
@@ -1,7 +1,4 @@
-use crate::{
-    config::{ConfigFile, CpuConfig, DiskConfig, GpuConfig, MemoryConfig, OsConfig, UptimeConfig},
-    error,
-};
+use crate::{config::ConfigFile, error};
 use regex::{Captures, Regex};
 use std::collections::HashMap;
 
@@ -17,16 +14,16 @@ macro_rules! insert_var {
     };
 
     ($m:expr, $s:expr, $c:expr) => {
-        $m.insert($s.name, Box::new(|| $s.value(Some(&$c))))
+        $m.insert($s.name, Box::new(|| $s.value($c)))
     };
 }
 
 pub fn get_info_lines(config: ConfigFile) -> Vec<String> {
-    let separator = &config.output.separator.unwrap();
+    let separator = &config.output.separator.as_ref().unwrap();
     let modules = &config.output.format;
 
     let mut output: Vec<String> = Vec::new();
-    let vars = init_vars();
+    let vars = init_vars(&config);
 
     for module in modules {
         if module.key.is_empty() {
@@ -43,36 +40,34 @@ pub fn get_info_lines(config: ConfigFile) -> Vec<String> {
 
         let parsed_key = parse_vars(&vars, &module.key);
         let parsed_content = parse_vars(&vars, &module.content);
-        println!("{}{separator}{}", parsed_key, parsed_content);
         output.push(format!("{}{separator}{}", parsed_key, parsed_content))
     }
 
     output
 }
 
-type ModuleVars<'a> = HashMap<&'a str, Box<dyn Fn() -> String>>;
+type ModuleVars<'a> = HashMap<&'a str, Box<dyn Fn() -> String + 'a>>;
 
-fn init_vars<'a>() -> ModuleVars<'a> {
-    let mut vars: ModuleVars = HashMap::new();
-    insert_var!(vars, os::OsVar::new(), OsConfig::default());
+fn init_vars<'a>(config: &'a ConfigFile) -> ModuleVars<'a> {
+    let mut vars: ModuleVars<'a> = HashMap::new();
+    insert_var!(vars, os::OsVar::new(), config.os.as_ref());
     insert_var!(vars, username::UserNameVar::new());
     insert_var!(vars, host::HostNameVar::new());
     insert_var!(vars, host::HostVar::new());
-    insert_var!(vars, memory::MemoryVar::new(), MemoryConfig::default());
-    insert_var!(vars, gpu::GpuVar::new(), GpuConfig::default());
-    insert_var!(vars, cpu::CpuVar::new(), CpuConfig::default());
-    insert_var!(vars, disk::DiskVar::new(), DiskConfig::default());
-    insert_var!(vars, uptime::UptimeVar::new(), UptimeConfig::default());
+    insert_var!(vars, memory::MemoryVar::new(), config.memory.as_ref());
+    insert_var!(vars, gpu::GpuVar::new(), config.gpu.as_ref());
+    insert_var!(vars, cpu::CpuVar::new(), config.cpu.as_ref());
+    insert_var!(vars, disk::DiskVar::new(), config.disk.as_ref());
+    insert_var!(vars, uptime::UptimeVar::new(), config.uptime.as_ref());
 
     vars
 }
 
-fn parse_vars<'a>(vars: &ModuleVars<'a>, content: &str) -> String {
+fn parse_vars<'a>(vars: &'a ModuleVars, content: &str) -> String {
     let re: Regex = Regex::new(r"\$\{([a-zA-Z]+)\}").unwrap();
 
     re.replace_all(content, |cap: &Captures| {
         let var = vars.get(&cap[1]);
-        println!("{}", &cap[1]);
         match var {
             Some(f) => f(),
             None => error::invalid_var(&content, &cap[1]),
