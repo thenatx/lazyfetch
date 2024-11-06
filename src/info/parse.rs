c6b5049f5ed51653f80ad02a4c722450e58728c8
NatProgramer <rodrigezsantiago40@gmail.com>
2024-11-05 22:34:08 -0500

    Solve clippy warnings on 'src/info/parse.rs'

--- src/info/parse.rs
+++ src/info/parse.rs
@@ -2,14 +2,14 @@ use super::ModuleVars;
 use crate::error;
 use regex::{Captures, Regex};
 
-pub fn parse_vars<'a>(vars: &'a ModuleVars, content: &str) -> String {
+pub fn parse_vars(vars: &ModuleVars, content: &str) -> String {
     let re: Regex = Regex::new(r"\$\{([a-zA-Z]+)\}").unwrap();
 
     re.replace_all(content, |cap: &Captures| {
         let var = vars.get(&cap[1]);
         match var {
             Some(f) => f(),
-            None => error::invalid_var(&content, &cap[1]),
+            None => error::invalid_var(content, &cap[1]),
         }
     })
     .to_string()
