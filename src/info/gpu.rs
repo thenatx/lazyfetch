6c4d341a4bda9d9085d45850074019d969a2589e
thenatx <rodrigezsantiago40@gmail.com>
2024-12-10 22:45:11 -0500

    Refactor the GPU module for avoid using regex and make it more readable

--- src/info/gpu.rs
+++ src/info/gpu.rs
@@ -1,9 +1,27 @@
+use std::collections::HashMap;
+use std::fs;
+
 use crate::config::file::GpuConfig;
-use regex::Regex;
-use std::process::Command;
+use crate::error::LazyfetchError;
 
 use super::ModuleVar;
-use crate::error::LazyfetchError;
+
+static DEFAULT_PCI_IDS: &str = include_str!("../assets/pci.ids");
+
+#[derive(Clone, Debug, Default)]
+struct Vendor {
+    name: String,
+    devices: HashMap<String, String>,
+}
+
+impl Vendor {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            devices: HashMap::new(),
+        }
+    }
+}
 
 pub struct GpuVar;
 
@@ -12,99 +30,102 @@ impl ModuleVar<GpuConfig> for GpuVar {
         String::from("gpu")
     }
 
+    // NOTE:
+    // i only use an integrated AMD gpu, so the names of other vendors can be wrong formatted because are not tested at all
+    // If you have another gpu and see mesy/wrong output please open an issue with an image of the output and your gpu name
     #[cfg(target_os = "linux")]
     fn value(self, cfg: Option<&GpuConfig>) -> Result<String, LazyfetchError> {
         let config = cfg.unwrap();
-        let lspci = {
-            let lspci_cmd = Command::new("sh").arg("-c").arg("lspci -mm").output();
-            String::from_utf8(lspci_cmd?.stdout).unwrap()
-        };
+        let ids = parse_pci_ids(DEFAULT_PCI_IDS);
+        let entries = fs::read_dir("/sys/class/drm")?;
+        let mut gpus: Vec<(String, String)> = Vec::new();
+        for entry in entries {
+            let (name, path) = if let Ok(e) = entry {
+                (e.file_name(), e.path())
+            } else {
+                continue;
+            };
 
-        let mut gpus = {
-            let mut to_return = Vec::new();
-            let lspci_lines = lspci.split('\n').collect::<Vec<&str>>();
-            let regex = Regex::new(
-                r#"(?i)"(.*?(?:Display|3D|VGA).*?)" "(.*?\[.*?\])" "(?:.*?\[(.*?)\])""#,
-            )?;
-            for line in &lspci_lines {
-                let captures = regex.captures(line);
-                if let Some(captures) = captures {
-                    to_return.push((
-                        String::from(captures.get(1).unwrap().as_str()),
-                        String::from(captures.get(2).unwrap().as_str()),
-                        String::from(captures.get(3).unwrap().as_str()),
-                    ));
-                }
+            let name = name.to_string_lossy();
+            let device_path = path.join("device");
+
+            if !name.starts_with("card")
+                || !fs::exists(device_path.join("device"))?
+                || !fs::exists(device_path.join("vendor"))?
+            {
+                continue;
             }
 
-            to_return
-        };
+            let vendor_id = fs::read_to_string(device_path.join("vendor"))?;
+            let device_id = fs::read_to_string(device_path.join("device"))?;
 
-        if gpus.len() >= 2
-            && gpus[0].1.to_lowercase().contains("intel")
-            && gpus[1].1.to_lowercase().contains("intel")
-        {
-            gpus.pop();
+            let vendor = ids.get(vendor_id[2..].trim()).unwrap();
+            let device = vendor.devices.get(device_id[2..].trim()).unwrap();
+
+            gpus.push((vendor.name.clone(), device.to_string()));
         }
 
-        let mut to_return = GpuStruct::new(String::new(), String::new());
-        for gpu in &mut gpus {
-            if gpu.1.to_lowercase().contains("advanced") {
-                let mut brand = gpu.1.clone();
-                let regex = Regex::new(r".*?AMD.*?ATI.*?")?;
-                brand = String::from(regex.replace_all(&brand, "AMD ATI"));
-
-                to_return = GpuStruct::new(
-                    gpu.2.clone(),
-                    brand
-                        .replace(['[', ']'], "")
-                        .replace("OEM", "")
-                        .replace("Advanced Micro Devices, Inc.", ""),
-                );
-                break;
-            } else if gpu.1.to_lowercase().contains("nvidea") {
-                to_return = GpuStruct::new(gpu.2.clone(), gpu.1.clone().replace(['[', ']'], ""));
-                break;
-            } else if gpu.1.to_lowercase().contains("intel") {
-                let mut brand = gpu.1.clone();
-                brand = {
-                    let regex = Regex::new(".*?Intel")?;
-                    String::from(regex.replace(&brand, "Intel"))
-                };
-                brand = brand.replace("(R)", "").replace("Corporation", "");
-                brand = {
-                    let regex = Regex::new(r" \(.*?")?;
-                    String::from(regex.replace_all(&brand, ""))
-                };
-                brand = brand.replace("Integrated Graphics Controller", "");
-                brand = {
-                    let regex = Regex::new(r".*?Xeon.*?")?;
-                    String::from(regex.replace(&brand, "Intel HD Graphics"))
-                };
-                brand = String::from(brand.trim());
-                if brand.is_empty() {
-                    brand = String::from("Intel HD Graphics");
-                }
-                to_return = GpuStruct::new(gpu.2.clone(), brand);
-                break;
-            }
-        }
-
+        let gpu = flat_gpu_name((&gpus[0].0, &gpus[0].1));
         if !config.show_brand.unwrap_or(false) {
-            return Ok(to_return.name);
+            return Ok(gpu.1);
         }
 
-        Ok(format!("{} {}", to_return.brand, to_return.name))
+        Ok(format!("{} {}", gpu.0, gpu.1))
     }
 }
 
-struct GpuStruct {
-    name: String,
-    brand: String,
+fn parse_pci_ids(id_list: &str) -> HashMap<String, Vendor> {
+    let mut vendors: HashMap<String, Vendor> = HashMap::new();
+
+    let mut vendor_id = "";
+    for line in id_list.split('\n') {
+        if line.starts_with('#') || line.trim().is_empty() {
+            continue;
+        }
+
+        if !line.starts_with('\t') {
+            let vendor_data = line.split_once(' ').unwrap(); // 0: vendor id   1: vendor name
+            vendor_id = vendor_data.0;
+            vendors.insert(vendor_data.0.to_string(), Vendor::new(vendor_data.1));
+        }
+
+        let device_data = line.split_once(' ').unwrap();
+        if let Some(vendor) = vendors.get_mut(&mut vendor_id.to_string()) {
+            vendor.devices.insert(
+                device_data.0.trim().to_string(),
+                device_data.1.trim().to_string(),
+            );
+        }
+    }
+
+    return vendors;
 }
 
-impl GpuStruct {
-    fn new(name: String, brand: String) -> Self {
-        Self { name, brand }
-    }
+fn flat_gpu_name(gpu: (&str, &str)) -> (String, String) {
+    let flated_name = match gpu {
+        g if g.0.contains("Advanced") => {
+            let brand = match g {
+                g if g.0.contains("[AMD/ATI]") => "AMD ATI",
+                g if g.0.contains("[ATI]") => "ATI",
+                g if g.0.contains("[AMD]") => "AMD",
+                _ => "",
+            };
+
+            let name = g.1.replace(['[', ']'], "");
+            (brand.trim().to_string(), name.trim().to_string())
+        }
+        g if g.1.contains("NVIDIA") => {
+            (g.0.replace("Corporation", ""), g.1.replace(['[', ']'], ""))
+        }
+        g if g.1.contains("Intel") => {
+            let name =
+                g.1.replace("(R)", "")
+                    .replace("Integrated Graphics Controller", "");
+
+            (g.0.replace("Corporation", ""), name)
+        }
+        _ => (gpu.0.to_string(), gpu.1.to_string()),
+    };
+
+    return flated_name;
 }
